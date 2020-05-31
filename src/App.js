9a0c38ebcd5cb21b89d104293071da7d02bac6cb
zhangqiling <1470166217@qq.com>
2020-05-31 22:46:55 +0800

    第一次

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id="App">
+      <header>
+        <div className="navbox">
+          <div className="logo">
+            <a href="">康惠军个人博客</a>
+          </div>
+          <ul className="list">
+            <li>网站首页</li>
+            <li>个人博客</li>
+            <li>网站制作</li>
+            <li>设计心得</li>
+            <li>时间轴</li>
+            <li>博客导航</li>
+            <li>留言</li>
+          </ul>
+          <div className="seatch">搜索</div>
+        </div>
       </header>
+      <article>
+        <div className="whitebg">
+          <div className="whitebg-list">
+            <ul>
+              <li>个人博客</li>
+              <li>CSS3|HTML5</li>
+              <li>网站建设</li>
+              <li>推荐工具</li>
+              <li>设计心得</li>
+            </ul>
+          </div>
+        </div>
+      </article>
     </div>
   );
 }
